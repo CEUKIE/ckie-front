5745ecc36000b163f6efb8df577c82e80053041c
Haesung <koh1260@naver.com>
2024-05-24 17:32:20 +0900

    feat: Chart 컴포넌트 분리

---
+++ src/components/Chart.tsx
@@ -0,0 +1,95 @@
+import React, {useEffect, useState} from 'react';
+import {Alert, Dimensions, View} from 'react-native';
+import {LineChart} from 'react-native-chart-kit';
+import theme from '../styles/theme';
+import Svg, {Text as SVGText} from 'react-native-svg';
+import {LineChartData} from 'react-native-chart-kit/dist/line-chart/LineChart';
+
+export interface ChartProps {
+  records: number[];
+  labels: string[];
+  yAxisSuffix: string;
+}
+
+const Chart = ({records, labels, yAxisSuffix}: ChartProps) => {
+  const [data, setData] = useState<LineChartData>({
+    labels,
+    datasets: [
+      {
+        data: [],
+      },
+    ],
+  });
+
+  useEffect(() => {
+    const tmpRecords: number[] = [];
+    records.forEach(record => tmpRecords.push(record));
+    setData({
+      ...data,
+      datasets: [
+        {
+          data: [...tmpRecords],
+        },
+        // y축 최댓값이 record의 최댓값보다 크게 하기 위한 투명한 점.
+        {
+          data: [Math.max(...tmpRecords) + 5],
+          color: () => 'transparent',
+          strokeWidth: 0,
+          withDots: false,
+        },
+      ],
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <View>
+      {data.datasets[0].data.length !== 0 && (
+        <LineChart
+          data={data}
+          width={Dimensions.get('window').width} // from react-native
+          height={260}
+          fromZero
+          yAxisSuffix={yAxisSuffix}
+          yAxisInterval={1} // optional, defaults to 1
+          onDataPointClick={({value}) => Alert.alert(value.toString())}
+          chartConfig={{
+            backgroundGradientFrom: theme.color.white,
+            backgroundGradientTo: theme.color.white,
+            fillShadowGradientFrom: theme.color.secondary,
+            fillShadowGradientTo: theme.color.secondary,
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: () => theme.color.secondary,
+            labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+            propsForDots: {
+              r: '5',
+              strokeWidth: '2',
+              stroke: theme.color.white,
+            },
+          }}
+          renderDotContent={({x, y, index, indexData}) => (
+            <Svg key={index} style={{position: 'absolute', top: 0, left: 0}}>
+              <SVGText
+                x={x}
+                y={y - 11}
+                fontSize="10"
+                fontWeight="bold"
+                textAnchor="middle">
+                {indexData}
+              </SVGText>
+            </Svg>
+          )}
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+      )}
+    </View>
+  );
+};
+
+export default Chart;
