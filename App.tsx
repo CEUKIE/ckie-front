f51efe90a86e2fe80a4a02c15c4e9d68b4c4252d
Haesung <koh1260@naver.com>
2024-05-28 13:35:28 +0900

    chore: react-query  의존성 추가

--- App.tsx
+++ App.tsx
@@ -5,14 +5,26 @@ import 'react-native-gesture-handler';
 
 import theme from './src/styles/theme';
 import RootNavigation from './src/navigations/RootNavigation';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 const App = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        throwOnError: true,
+        retry: 0,
+      },
+    },
+  });
+
   return (
-    <ThemeProvider theme={theme}>
-      <NavigationContainer independent>
-        <RootNavigation />
-      </NavigationContainer>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer independent>
+          <RootNavigation />
+        </NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
