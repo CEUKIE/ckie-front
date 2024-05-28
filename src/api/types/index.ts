203235f5b79cf66948ba3e4bcc8fc071bb1b2106
Haesung <koh1260@naver.com>
2024-05-29 05:53:39 +0900

    type: 로그인, 회원가입 타입 구현

---
+++ src/api/types/index.ts
@@ -0,0 +1,32 @@
+import {HttpStatusCode} from 'axios';
+
+export interface ResponseForm<T> {
+  status: HttpStatusCode;
+  result?: T;
+}
+
+export namespace AuthType {
+  export interface LoginRequest {
+    accessToken: string;
+  }
+
+  export interface SignupRequest {
+    accessToken: string;
+    avatarUrl: string;
+    nickname: string;
+    introduction: string;
+  }
+
+  export interface LoginResponse {
+    accessToken?: string;
+    isRegisterd?: boolean;
+  }
+
+  export interface SignupResponse {
+    accessToken: string;
+  }
+
+  export interface VerifyAccessTokenRepsonse {
+    isVerified: boolean;
+  }
+}
